420a85913bf4dd370139c688714e57e5a20d1861
Aike <60257865+Crtd-Code@users.noreply.github.com>
2021-02-05 12:49:23 +0800

    Update weater.py

--- weater.py
+++ weater.py
@@ -1,10 +1,1 @@
-import requests
-#推送标题
-text='天气推送'
-#推送内容
-desp='你好呀67897655768'
-#Server酱的SCKEY
-sckey = 'SCU120580T45a16bea7f3739f7654203489fbe69115fa8d55bbed50'#在发送消息页面可以找到
-url = 'https://sc.ftqq.com/%s.send?text=%s&desp=%s'%(sckey,text,desp)
-print(url)
-requests.get(url)
+
