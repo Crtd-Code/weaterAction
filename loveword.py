201fc7a66a1496272458e93016390fdea63da64f
Aike <60257865+Crtd-Code@users.noreply.github.com>
2021-04-16 21:43:47 +0800

    Update loveword.py

--- loveword.py
+++ loveword.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import random
 import os
 import time
-from fake_useragent import UserAgent
 from lxml import etree
 SKEY=os.environ.get('SKEY') #CoolPush酷推KEY
 ips = []  # 装载有效 IP
@@ -31,31 +30,40 @@ def getIP():
 
         print("正在准备IP代理，请稍后。。。")
 def getlovewords():
-    getIP()
+    # getIP()
     headers={
         'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Mobile Safari/537.36'
     }
     # 获取情话
     texts=[]
-    for page in range(1,20):
+    for page in range(1,2):
         time.sleep(3)
-        proxy = ips[random.randint(0, len(ips) - 1)]
-        print(proxy)
+        # proxy = ips[random.randint(0, len(ips) - 1)]
+        # print(proxy)
         url = 'https://www.duanwenxue.com/huayu/tianyanmiyu/list_{}.html'.format(page)
-        response = requests.get(url,headers=headers,proxies=proxy)
+        response = requests.get(url,headers=headers)
         soup=BeautifulSoup(response.text,'lxml')
         lovewordslist=soup.find('div',class_='list-short-article').find_all('a',target='_blank')
+        # print(lovewordslist)
         texts.extend([lovewordslist[i].text for i in range(len(lovewordslist))])
+
     todaywords = texts[random.randint(0, len(texts) - 1)]  # 随机选取其中一条情话
     return todaywords
 def CoolPush(info): #CoolPush酷推
     # cpurl = 'https://push.xuthus.cc/group/'+spkey   #推送到QQ群
     # cpurl = 'https://push.xuthus.cc/send/' + SKey  # 推送到个人QQ
     api='https://push.xuthus.cc/send/{}'.format(SKEY)
-    print(api)
     print(info)
-    requests.post(api, info.encode('utf-8'))
+    r=requests.post(api, info.encode('utf-8'))
+    if(r.status_code==200):
+        print("推送成功")
 if __name__ == '__main__':
-    info='晚安❤老婆\n'+getlovewords()
-    CoolPush(info)
-    print(1234)
+    str1='晚安❤老婆\n'+getlovewords()
+    str2='晚安❤老婆\n'+getlovewords()
+    str3='晚安❤老婆\n'+getlovewords()
+    print(str1)
+    print(str2)
+    print(str3)
+    CoolPush(str1)
+    CoolPush(str2)
+    CoolPush(str3)
